4fe8ecefcb68d13fb36e43df3553e67f22ec7793
SaurabhJagarn <saurabh.verma@jagrannewmedia.com>
2024-10-22 14:31:22 +0530

    fix code

--- src/Main.js
+++ src/Main.js
@@ -12,56 +12,14 @@
 
 'use client'
 
-import { useState } from 'react'
-import {
-  Bars3Icon} from '@heroicons/react/24/outline'
-import CmsButton from './base/Button'
-import Header from './components/Header/Header'
-import YoutubeEmbed from './components/Dashboard/YoutubeEmbed'
-import VideoInformationSection from './components/Dashboard/VideoInformationSection'
-import DialogBox from './components/Dashboard/DialogBox'
-import CmsTextInput from './base/TextInput'
+import Dashboard from './pages/Dashboard'
 
-
-
-// Main Dashboard Component
 export default function Main() {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
 
   return (
     <>
       <div>
-       
-        <DialogBox setSidebarOpen={setSidebarOpen} sidebarOpen={sidebarOpen}/>
-        <div className="lg:pl-72">
-          <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
-            <button type="button" onClick={() => setSidebarOpen(true)} className="-m-2.5 p-2.5 text-gray-700 lg:hidden">
-              <span className="sr-only">Open sidebar</span>
-              <Bars3Icon aria-hidden="true" className="h-6 w-6" />
-            </button>
-
-            {/* Profile dropdown */}
-            <div className='flex flex-row w-full justify-end'>
-              <Header />
-            </div>
-          </div>
-
-          {/* Main Form Section */}
-          <main className="py-10">
-            <div className="px-4 sm:px-6 lg:px-8">
-              <form>
-                <div className="space-y-12">
-                  <YoutubeEmbed />
-                  <VideoInformationSection />
-                </div>
-
-                <div className="mt-6 flex items-center justify-end gap-x-6">
-                  <CmsButton buttonTypeStyle={"primary"} buttonText={"Save Changes"}/>
-                </div>
-              </form>
-            </div>
-          </main>
-        </div>
+        <Dashboard />
       </div>
     </>
   )
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -14,18 +14,9 @@
 */
 'use client'
 
-import { useState } from 'react'
-import {
-  Bars3Icon} from '@heroicons/react/24/outline'
-import VideoInformationSection from '../components/Dashboard/VideoInformationSection'
-import CmsButton from '../base/Button'
-import YoutubeEmbed from '../components/Dashboard/YoutubeEmbed'
-import Header from '../components/Header/Header'
-import DialogBox from '../components/Dashboard/DialogBox'
 import DashboardComponent from '../components/Dashboard/DashBoard'
 
 export default function Dashboard() {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
   return (
     <>
     
